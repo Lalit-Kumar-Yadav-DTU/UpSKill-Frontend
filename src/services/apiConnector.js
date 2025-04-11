87bd6d1eca5ca691ffd72cdd29e8b0bf08b36e4c
Lalit Kumar Yadav <lalitkumaryadav14112003@gmail.com>
2025-04-11 22:20:52 +0530

    Configured axiosInstance with baseURL from env for Render

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -1,13 +1,31 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({});
+
+// export const apiConnector = (method, url, bodyData, headers, params) => {
+//   return axiosInstance({
+//     method: `${method}`,
+//     url: `${url}`,
+//     data: bodyData ? bodyData : null,
+//     headers: headers ? headers : null,
+//     params: params ? params : null,
+//   });
+// };
+
+
+
 import axios from "axios";
 
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
     method: `${method}`,
     url: `${url}`,
-    data: bodyData ? bodyData : null,
-    headers: headers ? headers : null,
-    params: params ? params : null,
+    data: bodyData || null,
+    headers: headers || null,
+    params: params || null,
   });
 };
